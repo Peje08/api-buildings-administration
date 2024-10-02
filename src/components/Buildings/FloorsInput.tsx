847e80230cb951cf9f1dfbd36b50afa15230750e
Guille <guillepazerrobidart@gmail.com>
2024-10-02 12:35:49 -0300

    delete eslint rules, add 0 value for inputs

--- src/components/Buildings/FloorsInput.tsx
+++ src/components/Buildings/FloorsInput.tsx
@@ -2,58 +2,61 @@ import { HStack, Text, NumberInput, NumberInputField, Checkbox } from '@chakra-u
 import { strings } from '../../constants/strings'
 
 interface FloorsInputProps {
-  pisos: number
-  handlePisosChange: (value: number) => void
-  localChecked: boolean
-  setLocalChecked: React.Dispatch<React.SetStateAction<boolean>>
-  pisosIguales: boolean
-  setPisosIguales: React.Dispatch<React.SetStateAction<boolean>>
+	pisos: number
+	handlePisosChange: (value: number) => void
+	localChecked: boolean
+	setLocalChecked: React.Dispatch<React.SetStateAction<boolean>>
+	pisosIguales: boolean
+	setPisosIguales: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const FloorsInput: React.FC<FloorsInputProps> = ({
-  pisos,
-  handlePisosChange,
-  localChecked,
-  setLocalChecked,
-  pisosIguales,
-  setPisosIguales
+	pisos,
+	handlePisosChange,
+	localChecked,
+	setLocalChecked,
+	pisosIguales,
+	setPisosIguales
 }) => {
-  return (
-    <HStack width="100%" spacing={6} alignItems="center" mb={4}>
-      <HStack>
-        <Text>{strings.floors}</Text>
-        <NumberInput
-          min={1}
-          max={100}
-          value={pisos}
-          onChange={(valueString) => handlePisosChange(Number(valueString))}
-          width="50%"
-          isRequired
-          aria-required="true"
-        >
-          <NumberInputField />
-        </NumberInput>
-      </HStack>
+	return (
+		<HStack width='100%' spacing={6} alignItems='center' mb={4}>
+			<HStack>
+				<Text>{strings.floors}</Text>
+				<NumberInput
+					min={0}
+					max={100}
+					value={pisos}
+					onChange={(valueString) => {
+						const value = parseInt(valueString, 10)
+						handlePisosChange(isNaN(value) ? 0 : value)
+					}}
+					width='50%'
+					isRequired
+					aria-required='true'
+				>
+					<NumberInputField />
+				</NumberInput>
+			</HStack>
 
-      <HStack>
-        <Checkbox
-          isChecked={localChecked}
-          onChange={(e) => setLocalChecked(e.target.checked)}
-        >
-          {strings.premise}
-        </Checkbox>
-      </HStack>
+			<HStack>
+				<Checkbox
+					isChecked={localChecked}
+					onChange={(e) => setLocalChecked(e.target.checked)}
+				>
+					{strings.premise}
+				</Checkbox>
+			</HStack>
 
-      <HStack>
-        <Checkbox
-          isChecked={pisosIguales}
-          onChange={(e) => setPisosIguales(e.target.checked)}
-        >
-          {strings.sameFloors}
-        </Checkbox>
-      </HStack>
-    </HStack>
-  )
+			<HStack>
+				<Checkbox
+					isChecked={pisosIguales}
+					onChange={(e) => setPisosIguales(e.target.checked)}
+				>
+					{strings.sameFloors}
+				</Checkbox>
+			</HStack>
+		</HStack>
+	)
 }
 
 export default FloorsInput
--- src/components/Buildings/index.tsx
+++ src/components/Buildings/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { VStack, HStack, } from '@chakra-ui/react'
+import { VStack, HStack } from '@chakra-ui/react'
 import { colors } from '../../constants/colors'
 import StreetInput from './StreetInput'
 import TowerAccordion from './TowerAccordion'
@@ -7,67 +7,65 @@ import ActionButtons from './ActionButtons'
 import BuildingNumberInput from './NumberInputField'
 
 interface BuildingConfigurationProps {
-  onCancel: () => void
+	onCancel: () => void
 }
 
-const BuildingConfiguration: React.FC<BuildingConfigurationProps> = ({
-  onCancel,
-}) => {
-  const [localChecked, setLocalChecked] = useState(false)
-  const [pisos, setPisos] = useState(1)
-  const [isLetras, setIsLetras] = useState(false)
-  const [pisosIguales, setPisosIguales] = useState(false)
-  const [pisosValues, setPisosValues] = useState<string[]>(Array(3).fill(''))
+const BuildingConfiguration: React.FC<BuildingConfigurationProps> = ({ onCancel }) => {
+	const [localChecked, setLocalChecked] = useState(false)
+	const [pisos, setPisos] = useState(0)
+	const [isLetras, setIsLetras] = useState(false)
+	const [pisosIguales, setPisosIguales] = useState(false)
+	const [pisosValues, setPisosValues] = useState<string[]>(Array(3).fill(''))
 
-  const handlePisoChange = (value: string, index: number) => {
-    if (pisosIguales && index === 0) {
-      const newValues = Array(pisos).fill(value)
-      setPisosValues(newValues)
-    } else {
-      const newValues = [...pisosValues]
-      newValues[index] = value
-      setPisosValues(newValues)
-    }
-  }
+	const handlePisoChange = (value: string, index: number) => {
+		if (pisosIguales && index === 0) {
+			const newValues = Array(pisos).fill(value)
+			setPisosValues(newValues)
+		} else {
+			const newValues = [...pisosValues]
+			newValues[index] = value
+			setPisosValues(newValues)
+		}
+	}
 
-  const handlePisosChange = (value: number) => {
-    setPisos(value)
-    setPisosValues(Array(value).fill(''))
-  }
+	const handlePisosChange = (value: number) => {
+		setPisos(value)
+		setPisosValues(Array(value).fill(''))
+	}
 
-  return (
-    <VStack
-      spacing={6}
-      align="center"
-      width="100%"
-      p={15}
-      bg={colors.inputBackground}
-      border="1px"
-      borderColor={colors.verticalDivider}
-      borderRadius="4px"
-    >
-      <HStack width="90%" spacing={8}>
-        <StreetInput />
-        <BuildingNumberInput />
-      </HStack>
+	return (
+		<VStack
+			spacing={6}
+			align='center'
+			width='100%'
+			p={15}
+			bg={colors.inputBackground}
+			border='1px'
+			borderColor={colors.verticalDivider}
+			borderRadius='4px'
+		>
+			<HStack width='90%' spacing={8}>
+				<StreetInput />
+				<BuildingNumberInput />
+			</HStack>
 
-      {/* Acordeón de la Torre 1 */}
-      <TowerAccordion
-        pisos={pisos}
-        handlePisosChange={handlePisosChange}
-        localChecked={localChecked}
-        setLocalChecked={setLocalChecked}
-        pisosIguales={pisosIguales}
-        setPisosIguales={setPisosIguales}
-        isLetras={isLetras}
-        setIsLetras={setIsLetras}
-        pisosValues={pisosValues}
-        handlePisoChange={handlePisoChange}
-      />
+			{/* Acordeón de la Torre 1 */}
+			<TowerAccordion
+				pisos={pisos}
+				handlePisosChange={handlePisosChange}
+				localChecked={localChecked}
+				setLocalChecked={setLocalChecked}
+				pisosIguales={pisosIguales}
+				setPisosIguales={setPisosIguales}
+				isLetras={isLetras}
+				setIsLetras={setIsLetras}
+				pisosValues={pisosValues}
+				handlePisoChange={handlePisoChange}
+			/>
 
-      <ActionButtons onCancel={onCancel} />
-    </VStack>
-  )
+			<ActionButtons onCancel={onCancel} />
+		</VStack>
+	)
 }
 
 export default BuildingConfiguration
